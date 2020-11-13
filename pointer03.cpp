c5ecda0fb028ab74d3219fd3ad8bce26351a3e6c
yuhwanwoo <6514687@naver.com>
2020-11-14 01:14:06 +0900

    pointer

---
+++ pointer03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int* pA;
+
+    a=5;
+    pA=&a;
+
+    cout<<"변수 a의 값은"<<a<<"입니다.\n";
+    cout<<"변수 a의 수소(&a)는"<<&a<<"입니다.\n";
+    cout<<"포인터 pA의 값은"<<pA<<"입니다.\n";
+    cout<<"*pA의 값은 "<<*pA<<"입니다.\n";
+
+    return 0;
+}
---
+++ pointer_init.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+
+    a=5;
+
+    cout<<"변수 a의 값은"<<a<<"입니다.\n";
+    cout<<"변수 a의 주소는"<<&a<<"입니다.\n";
+
+    return 0;
+}
