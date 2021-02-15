125a05eaa9fa0c4925e3e261a42351db82a14a56
yuhwanwoo <6514687@naver.com>
2021-02-15 22:12:59 +0900

    queue baekjoon

---
+++ bark/0x06/baekjoon10845.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX=1000005;
+int dat[MX];
+int head, tail;
+
+void push(int x){
+    dat[tail++]=x;
+}
+
+void pop(){
+    head;
+}
+
+int front(){
+    return dat[head];
+}
+
+int back(){
+    return dat[tail-1];
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    while(n--){
+        string q;
+        cin>>q;
+        if(q=="push"){
+            int val;
+            cin>>val;
+            push(val);
+        }
+        else if(q=="pop"){
+            if(tail==head)
+                cout<<-1<<'\n';
+            else{
+                cout<<front()<<'\n';
+                pop();
+            }
+        }
+        else if(q=="size"){
+            cout<<tail-head<<"\n";
+        }
+        else if(q=="empty"){
+            cout<<(tail==head)<<"\n";
+        }
+        else if(q=="front"){
+            if(tail==head)
+                cout<<-1<<"\n";
+            else cout<<front()<<"\n";
+        }
+        else{ //back
+            if(tail==head)
+                cout<<-1<<"\n";
+            else cout<<back()<<"\n";
+        }
+    }
+
+}
