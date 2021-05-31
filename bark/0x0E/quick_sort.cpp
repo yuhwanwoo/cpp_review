69e952165778f142f4c0f9328962b38819f863cf
yuhwanwoo <6514687@naver.com>
2021-06-01 00:10:58 +0900

    sorting

---
+++ bark/0x0E/quick_sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n=10;
+int arr[1000001]={};
+
+void quick_sort(int st,int en){
+    if(en<=st+1) return;
+    int pivot = arr[st];
+    int l = st+1;
+    int r = en-1;
+    while(1){
+        while (l<=r && arr[l] <= pivot) l++;
+        while (l<=r && arr[r] >= pivot) r--;
+        if(l>r) break;
+        swap(arr[l],arr[r]);
+    }
+    swap(arr[st],arr[r]);
+    quick_sort(st,r);
+    quick_sort(r+1,en);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    quick_sort(0,n);
+    for(int i=0;i<n;i++) cout<< arr[i]<<' ';
+}
